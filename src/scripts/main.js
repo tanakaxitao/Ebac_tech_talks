b6cab0cf24e24067ae365b819f89bdd6dcad7b3b
gabriel lopes <tanakacvr@gmail.com>
2023-05-23 16:12:02 -0300

    criando a contagem regressiva

---
+++ src/scripts/main.js
@@ -0,0 +1,39 @@
+AOS.init();
+const dataDoEvento = new Date("may 30, 2023 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function () {
+  const agora = new Date();
+  const timeStampAtual = agora.getTime();
+
+  const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+  const diaEmMS = 1000 * 60 * 60 * 24;
+  const horaEmMS = 1000 * 60 * 60;
+  const minutoEmMs = 1000 * 60;
+
+  const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMS);
+  const horasAteOEvento = Math.floor(
+    (distanciaAteOEvento % diaEmMS) / horaEmMS
+  );
+
+  const minutosAteOEvento = Math.floor(
+    (distanciaAteOEvento % diaEmMS) / minutoEmMs
+  );
+  const segundosAteOEvento = Math.floor(
+    (distanciaAteOEvento % minutoEmMs) / 1000
+  );
+
+  console.log(diasAteOEvento);
+  console.log(horasAteOEvento);
+  console.log(segundosAteOEvento);
+
+  document.getElementById(
+    "contador"
+  ).innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}min ${segundosAteOEvento}s`;
+
+  if (distanciaAteOEvento < 0) {
+    clearInterval(contaAsHoras);
+    document.getElementById("contador").innerHTML = `Evento expirado`;
+  }
+}, 1000);
